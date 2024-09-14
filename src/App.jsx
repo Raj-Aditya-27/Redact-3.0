3950fb72577170e76ddc5f206b8fffe3592edca1
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-09-15 05:28:00 +0530

    logo added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
       {/* <Home/> */}
-      <Document/>
+      <Document/> 
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import img from "../assets/logo.png"
 
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="logo">Redact</div>
+      <div className="logo">
+        <img src={img} alt="" />
+      </div>
       <div className="about">About</div>
     </div>
   );
