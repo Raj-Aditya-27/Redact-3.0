9aa617ad4c7b5eac65a56008aecb06eb35ead02e
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-09-15 05:46:07 +0530

    Loading component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 import "./App.css";
-import Home from "./components/Home"
+import Home from "./components/Home";
 import Document from "./components/Document";
+import Loading from "./components/Loading";
+import { CSSTransition } from "react-transition-group";
+import { useState,useEffect } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="app">
-      {/* <Home/> */}
-      <Document/> 
-    </div>
-  )
+    <>
+      <>
+        <CSSTransition
+          in={isLoading}
+          timeout={300}
+          classNames="fade"
+          unmountOnExit
+        >
+          <Loading />
+        </CSSTransition>
+        <CSSTransition
+          in={!isLoading}
+          timeout={300}
+          classNames="fade"
+          unmountOnExit
+        >
+          <Home />
+        </CSSTransition>
+      </>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <>
+      <section>
+        <h1>
+          <span>R</span>
+          <span>e</span>
+          <span>d</span>
+          <span>a</span>
+          <span>c</span>
+          <span>t</span>
+        </h1>
+      </section>
+    </>
+  );
+}
+
+export default Loading;
